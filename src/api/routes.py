ff895d414a89e83deb93d9954b501d4d3d9bffc2
RodriMaldonadoMedina <maldonadomedinarodrigo@gmail.com>
2023-01-10 14:27:20 +0000

    borrado de una route innecesaria

--- src/api/routes.py
+++ src/api/routes.py
@@ -11,16 +11,6 @@ from slugify import slugify
 
 api = Blueprint('api', __name__)
 
-## REVISAR PARA BORRAR TODAS LAS FUNCIONES QUE VENIAN EN EL boilerplate ##
-
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-    return jsonify(response_body), 200
-
 
 ################################################################################
 #                                   signup                                     #
